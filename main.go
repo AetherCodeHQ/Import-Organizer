5fd6a916edcdc549f257f31ddad56976f87e1b4a
AetherCodeHQ <noreply@users.noreply.github.com>
2026-08-24 15:46:29 +0300

    v2.1.0: unique implementation replaces generic stub

--- main.go
+++ main.go
@@ -1,40 +1,44 @@
-
 package main
 
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"sort"
 	"strings"
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("usage: Import-Organizer <bytes|lines|reverse|upper|lower>")
+		fmt.Println("usage: import-organizer <file.go>")
 		os.Exit(1)
 	}
-	mode := os.Args[1]
-	in := ""
-	if len(os.Args) > 2 {
-		in = os.Args[2]
+	data, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
-	lines := strings.Split(in, "\n")
-	switch mode {
-	case "lines":
-		fmt.Println(len(lines))
-	case "bytes":
-		fmt.Println(len(in))
-	case "upper":
-		fmt.Println(strings.ToUpper(in))
-	case "lower":
-		fmt.Println(strings.ToLower(in))
-	case "reverse":
-		runes := []rune(in)
-		for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-			runes[i], runes[j] = runes[j], runes[i]
+	// match "package/path" or "pkgname" inside import blocks
+	re := regexp.MustCompile(`"([^"]+)"`)
+	matches := re.FindAllStringSubmatch(string(data), -1)
+	var stdlib, third []string
+	for _, m := range matches {
+		p := m[1]
+		if strings.Contains(p, ".") {
+			third = append(third, p)
+		} else {
+			stdlib = append(stdlib, p)
 		}
-		fmt.Println(string(runes))
-	default:
-		fmt.Fprintln(os.Stderr, "unknown mode:", mode)
-		os.Exit(1)
 	}
+	sort.Strings(stdlib)
+	sort.Strings(third)
+	fmt.Println("=== stdlib ===")
+	for _, p := range stdlib {
+		fmt.Println(p)
+	}
+	fmt.Println("=== third-party ===")
+	for _, p := range third {
+		fmt.Println(p)
+	}
+	fmt.Printf("\n%d stdlib, %d third-party\n", len(stdlib), len(third))
 }
